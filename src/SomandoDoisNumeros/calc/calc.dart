36964fd4dc016858e5213d602b0a7e4a83ff80aa
Bruno Lemes <bruno.santos@climatempo.io>
2022-08-13 22:11:14 -0300

    feat: added feature sum exercise 01

---
+++ src/SomandoDoisNumeros/calc/calc.dart
@@ -0,0 +1,7 @@
+class SomandoDoisNumeros {
+  double num = 0, num2 = 0;
+
+  SomandoDoisNumeros(this.num, this.num2);
+
+  void somar() => print(num + num2);
+}
---
+++ src/SomandoDoisNumeros/main.dart
@@ -0,0 +1,5 @@
+import "./calc/calc.dart";
+
+void main(List<String> arguments) {
+  SomandoDoisNumeros(20, 30).somar();
+}
